1927e6cb1ca01aefecfe44cfbf51c945062ba9a0
Santi Espinosa <saigesp@gmail.com>
2021-06-29 14:52:16 +0200

    add password recovery email

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -55,7 +55,7 @@
               <button
                 type="button"
                 class="btn"
-                :disabled="!isValidForm"
+                :disabled="!isValidForm || isLoading"
                 @click="submit()">{{$t('login.sign_in')}}</button>
             </div>
 
@@ -63,7 +63,7 @@
               <button
                 type="button"
                 class="btn"
-                :disabled="!isValidForm"
+                :disabled="!isValidForm || isLoading"
                 @click="submit()">{{$t('login.create_account')}}</button>
             </div>
 
@@ -73,6 +73,9 @@
         <div class="login__footer login__footer--signin" v-show="!isRegistering">
           <span>{{$t('login.not_registered')}} </span>
           <a @click="isRegistering = true">{{$t('login.not_registered_action')}}</a>.
+          <p class="login__recover">
+            {{$t('login.recover_password_instructions')}}:<br>{{$t('project.tenant_email')}}
+          </p>
         </div>
 
         <div class="login__footer login__footer--register" v-show="isRegistering">
@@ -100,6 +103,7 @@ export default {
   name: 'Login',
   data() {
     return {
+      isLoading: false,
       isRegistering: false,
       isWrongLogin: false,
       isRegistered: false,
@@ -149,6 +153,7 @@ export default {
       }
     },
     login() {
+      this.isLoading = true;
       this.$store.dispatch('login', {
         email: this.email,
         password: this.password,
@@ -160,9 +165,12 @@ export default {
         }
       }).catch(() => {
         this.wrongLogin();
+      }).finally(() => {
+        this.isLoading = false;
       });
     },
     register() {
+      this.isLoading = true;
       this.$store.dispatch('register', {
         email: this.email,
         password1: this.password,
@@ -172,6 +180,8 @@ export default {
       }).catch(() => {
         this.password = '';
         this.password2 = '';
+      }).finally(() => {
+        this.isLoading = false;
       });
     },
     wrongLogin() {
@@ -197,6 +207,12 @@ export default {
     padding: 20px;
     border-radius: 6px;
     box-shadow: 0 7px 14px 0 rgba(60,66,87,.1), 0 3px 6px 0 rgba(0,0,0,.07);
+  }
+
+  &__recover {
+    margin-top: 20px;
+    opacity: 0.6;
+    font-size: 14px;
   }
 
   &__footer {
